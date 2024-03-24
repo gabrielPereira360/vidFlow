dec0d1178990adb0e5e6cf1ee803f9ea827e0055
Gabriel Pereira <gabriel.obom01@gmail.com>
2024-03-24 19:45:33 -0300

    v3

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# vidFlow
-Projeto desenvolvido para implementar funções avançadas no Javascript para manipulação de dados via API 
+
+# Olá, bem vindo ao projeto desenvolvido na Formação Javascript da Alura
+### Projeto desenvolvido para implementar funções avançadas no Javascript para manipulação de dados via API em JSON
+
+## Prévias
+
+<div>
+  <div>
+    <h3>Desktop:</h3> 
+       <img src="#" width="500px" alt="Visualização em Desktop"/>
+  </div>
+  
+  <div>
+  <h3>Tablets:</h3> 
+    <img src="#" width="300px" alt="Visualização em Tablet"/>
+  </div>
+  
+  <div>
+  <h3>Mobile:</h3>
+    <img src="#" width="200px" alt="Visualização em Mobile"/>
+  </div>
+</div>
+
+## Ferramentas utilizadas:
+
+* HTML
+
+* CSS
+
+* Javascript
+    * Json-Server (Para uso de API)
+
+## Feito por:
+
+### Gabriel Pereira
+
+### Link da prévia: #
+### Linkedin: https://www.linkedin.com/in/gabriel-martins2003/
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const containerVideos = document.querySelector('.videos__container'); // Obter identificador da lista de vídeos
 
 async function buscarEMostrarVideos() {
-    try{
+    try {
         const busca = await fetch('http://localhost:3000/videos') // Obter a lista de vídeos
         const videos = await busca.json(); // Tranforma a promisse em um JSON
-            videos.forEach((video) => {
+        videos.forEach((video) => {
 
-                if (video.categoria == ""){
-                    throw new Error('Vídeo não possui categoria');
-                }
+            if (video.categoria == "") {
+                throw new Error('Vídeo não possui categoria');
+            }
 
-                containerVideos.innerHTML += `
+            containerVideos.innerHTML += `
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allow fullscreen></iframe>
                     <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem} alt="Logo do canal"
+                        <img class="img-canal" src="${video.imagem} alt="Logo do canal"</img>
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 <li>        
                 `
-                });
-    }catch(error){
+        });
+    } catch (error) {
         containerVideos.innerHTML = `
             <p style="color: red; margin: 15px auto; font-size: 24px;">Erro ao carregar os vídeos. Código do erro: ${error}</p>
         `;
@@ -31,5 +32,33 @@ async function buscarEMostrarVideos() {
 buscarEMostrarVideos();
 
 
+const barraDePesquisa = document.querySelector('.pesquisar__input');
 
+barraDePesquisa.addEventListener('input', filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item'); // Obtem a lista de vídeos
+    const valorFiltro = barraDePesquisa.value.toLowerCase(); // Obtem o item da barra de pesquisa em minusculo
+
+    videos.forEach((video) => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        video.style.display = valorFiltro ? (titulo.includes(valorFiltro) ? 'block' : 'none') : 'block';
+    });
+}
+
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute('name'); // Captura a categoria de cada botao
+    botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll('.videos__item'); // Obtem a lista de vídeos
+    const nomeDaCategoria = filtro.toLowerCase();
+    videos.forEach((video) => {
+        const categoriaVideo = video.querySelector('.categoria').textContent.toLowerCase();
+        video.style.display = ((!categoriaVideo.includes(nomeDaCategoria)) && (nomeDaCategoria != 'tudo')) ? 'none' : 'block';
+    });
+}
 
